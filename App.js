c1c75b4288d60c5900ff3da4eb73153aad47e61d
andreepratama27 <andreepratama27@gmail.com>
2017-12-06 21:50:59 +0800

    edit style

--- App.js
+++ App.js
@@ -1,29 +1,62 @@
 import Navbar from './components/Navbar'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const App = () => {
-    return (
-        <div className="wrapper">
-            <Navbar />
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            joke: [],
+            uri: 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke'
+        }
+        this._generateJokes = this._generateJokes.bind(this)
+    }
 
-            <div className="column">
-                <div className="content">
-                    <div className="text">
-                        Andre Pratama
+    componentDidMount() {
+        this._generateJokes()
+    } 
+
+    _generateJokes() {
+        axios.get(this.state.uri)
+            .then((res) => {
+                this.setState({
+                    joke: res.data
+                })
+            })
+    }
+
+    render() {
+        const { joke } = this.state
+
+        return (
+            <div className="wrapper">
+                <Navbar />
+
+                <div className="column">
+                    <div className="content">
+                        <div className="setup">
+                            { joke.setup }
+                        </div>
+                        <div className="punchline">
+                            { joke.punchline }
+                        </div>
+                        <div className="type">
+                            <i>Joke type - { joke.type }</i>
+                        </div>
+                    </div>
+                </div>
+                
+
+                <div className="">
+                    <div className="bottom-section">
+                        <button className="button is-info" onClick={ this._generateJokes }>
+                            Generate Jokes
+                        </button>
                     </div>
                 </div>
             </div>
-            
-
-            <div className="">
-                <div className="bottom-section">
-                    <button className="button is-info">
-                        Generate Jokes
-                    </button>
-                </div>
-            </div>
-            
-        </div>
-    )
+        )
+    }
 }
 
 export default App
